eab28d120fb956e0b911f59accce7876c51006c9
tdrk18 <tdrk18@gmail.com>
2019-08-04 15:05:03 +0900

    Create a row view

---
+++ Landmarks/Landmarks/Supporting Views/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by tdrk18 on 2019/08/04.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+        }
+    }
+}
+
+#if DEBUG
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
+#endif
